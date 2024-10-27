29a43654c16b8c9f264a5865d6d87744ab18e0d4
Andrea Pollastri <scrivi@andreapollastri.net>
2024-10-27 22:08:21 +0100

    Fix

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -42,7 +42,7 @@
     </div>
 </div>
 
-<div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
+<div class="bg-gray-200 bg-opacity-25 grid grid-cols-1">
     @foreach ($repositories as $repository)
         <div class="p-6">
             <div class="flex items-center">
